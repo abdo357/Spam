7d44dce07f833e271304bfd04d9d983d26853135
abdo357 <49820933+abdo357@users.noreply.github.com>
2019-11-13 16:42:34 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ console.log("Scrpit By Dream");
 
 
 client.on("ready", () => {
-let channel =     client.channels.get("639428699405680651")
+let channel =     client.channels.get("644184053435727872")
 setInterval(function() {
 channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
 }, 30)
