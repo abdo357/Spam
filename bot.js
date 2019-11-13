5f7fd60f381772fa64a27484894309ea4cb7983b
abdo357 <49820933+abdo357@users.noreply.github.com>
2019-11-13 16:41:11 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("639428699405680651")
+setInterval(function() {
+channel.send(` and are you abn 4rmota ? يابن كوم شكاير الاف ءح الدولية كسم اي حد يعترض طريقي`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
